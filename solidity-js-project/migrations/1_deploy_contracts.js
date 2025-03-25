4d2e6af8ce76e69772fc24bb830b6ce2223baa31
Raj_V <114798341+RjyavardhanSingh@users.noreply.github.com>
2025-03-25 17:54:20 +0530

    Delete solidity-js-project/migrations directory

--- solidity-js-project/migrations/1_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const MyContract = artifacts.require("MyContract");
-
-module.exports = function (deployer) {
-  // Deploy with name and initial value parameters
-  deployer.deploy(MyContract, "BlockchainSimulation", 100);
-};
